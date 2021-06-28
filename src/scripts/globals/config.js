b961f1f227e02895263a8780a8f267f8d9040a2e
Pratama Lutfi <pratamalutfi60@gmail.com>
2021-06-28 14:48:31 +0700

    Set websocket and notification

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    // cache pending
-
     BASE_URL: 'https://restaurant-api.dicoding.dev',
     DATABASE_NAME: "feed-app-database",
     DATABASE_VERSION: 1,
     OBJECT_STORE_NAME: "restaurants",
+    CACHE_NAME: new Date().toISOString(),
+    WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 };
    
 export default CONFIG;
---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,20 @@
+import NotificationHelper from './notification-helper';
+
+const WebSocketInitiator = {
+    init(url) {
+        const webSocket = new WebSocket(url);
+        webSocket.onmessage = this._onMessageHandler;
+    },
+
+    _onMessageHandler(message) {
+        const movie = JSON.parse(message.data);
+        NotificationHelper.sendNotification({
+            title: `${movie.title} is on cinema!`,
+            options: {
+                body: movie.overview,
+            },
+        });
+    },
+};
+
+export default WebSocketInitiator;
