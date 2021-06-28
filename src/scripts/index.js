9fa9dcdfe53980d78bb135adcf6756189fb1a5e9
Pratama Lutfi <pratamalutfi60@gmail.com>
2021-06-28 13:57:11 +0700

    setting service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 // import TOP_RESTAURANTS from '../DATA.json';
 
@@ -19,6 +20,7 @@ window.addEventListener('hashchange', () => {
  
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
 
 
